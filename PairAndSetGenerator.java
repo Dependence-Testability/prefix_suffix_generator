82468a361365fd4689d3d928ba2704189f6c796c
Yasirmod17 <mibrahim17@amherst.edu>
2016-12-15 16:10:11 -0500

    Start Generation

--- PairAndSetGenerator.java
+++ PairAndSetGenerator.java
@@ -10,44 +10,108 @@ About: Given two integers, m , n (m=length of prefix, n= length of suffix) and a
 */
 import java.util.*;
 import java.util.Arrays;
+import java.io.*;
 
 public class PairAndSetGenerator
 {
-	public static void main(String[] args)
+	//Test purposes only
+    public static void main(String[] args) 
 	{
-		//ArrayList<Integer> verts = new ArrayList<Integer>();
-		int prefix_length = 3;
-		int suffix_length = 3;
-		
-		int[] verts = {1, 2, 3, 4, 5,6,7,8,9,10};//11,12,13,14,15,16,17,18,19,20};
-		int number_of_verts = verts.length;
-		generatePairAndSet(verts,prefix_length+suffix_length);
+		// ArrayList<Integer> verts = new ArrayList<Integer>();
+		// int prefix_length = 2;
+		// int suffix_length = 2;
+		// Graph<Integer> graph = constructGraph(verts);
+
+		// int[] vertices = new int[verts.size()]; //11,12,13,14,15,16,17,18,19,20;
+  //       //verts.toArray(vertices);
+  //       for(int j=0; j<verts.size();j++)
+  //       {
+  //           vertices[j] = verts.get(j);
+  //       }
+		// int number_of_verts = verts.size();
+		// generatePairAndSet(vertices,prefix_length+suffix_length);
+        startGeneration(4, 1, 2);
+	}
+    public static void startGeneration(int prefSufLength, int source, int dest)
+    {
+        ArrayList<Integer> verts = new ArrayList<Integer>();
+        Graph<Integer> graph = constructGraph(verts);
+
+        int[] vertices = new int[verts.size()]; //11,12,13,14,15,16,17,18,19,20;
+        for(int j=0; j<verts.size();j++)
+        {
+            vertices[j] = verts.get(j);
+        }
+        int number_of_verts = verts.size();
+        //System.out.println(number_of_verts);
+        generatePairAndSet(vertices,prefSufLength, source, dest);
+    }
+
+    public static Graph constructGraph(ArrayList<Integer> verts)
+    {
+        File original = new File("original.txt");
+        Graph<Integer> originalGraph = new Graph<Integer>();
+        try
+        {
+            Scanner in = new Scanner(original);
+            
+            //ArraysList<Integer> verts= new ArrayList<Integer>(); 
+            while(in.hasNextLine())
+            {
+                String line  = in.nextLine();
+                String[] lineArray = line.split(" ");
+                verts.add(Integer.parseInt(lineArray[0]));
+                for(int i = 1; i<lineArray.length; i++)
+                {
+                    originalGraph.addEdge(Integer.parseInt(lineArray[0]), Integer.parseInt(lineArray[i]));
+                }
+            }
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
+        return originalGraph;
+    }
+	public static void generatePairAndSet(int[] verts, int setLength, int source, int dest)
+	{
+		generateCombination(verts,verts.length,2,setLength, source, dest);
 	}
 
-	public static void generatePairAndSet(int[] verts, int setLength)
-	{
-		generateCombination(verts,verts.length,2,setLength);
-	}
-
-	public static void combinationUtil(int[] arr, int[] data, int start,int end, int index, int r,int setLength)
+	public static void combinationUtil(int[] arr, int[] data, int start,int end, int index, int r,int setLength, int source, int dest)
     {
         if (index == r)
         {
             //Sice its a pair, generate all sets of length pref+suff-2
-            generateAndPrintSet(arr,data,setLength-r);
+            if((data[0]==source) && (data[1]==dest))
+            {
+                generateAndPrintSet(arr,data,setLength-r);
+            }
+            else if((data[1]==source) && (data[0]==dest))
+            {
+                int[] tempdata = new int[data.length];
+                int f =0;
+                for (int l=data.length-1; l>=0; l--)
+                {
+                    tempdata[f] = data[l];
+                    f++;
+                }
+                generateAndPrintSet(arr, tempdata, setLength-r);
+            }
             return;
         }
         for (int i=start; i<=end && end-i+1 >= r-index; i++)
         {
             data[index] = arr[i];
-            combinationUtil(arr, data, i+1, end, index+1, r,setLength);
+            combinationUtil(arr, data, i+1, end, index+1, r,setLength, source, dest);
         }
     }
  
-    public static void generateCombination(int[] arr, int n, int r, int setLength)
+    public static void generateCombination(int[] arr, int n, int r, int setLength, int source, int dest)
     {
         int[] data=new int[r];
-        combinationUtil(arr, data, 0, n-1, 0, r,setLength);
+        combinationUtil(arr, data, 0, n-1, 0, r,setLength, source,dest);
     }
 
     public static void generateAndPrintSet(int[] arr,int[] data,int actual_setLength)
@@ -93,10 +157,10 @@ public class PairAndSetGenerator
     public static void printSourceDestAndSet(int[] setToExclude, int[] source_dest)
     {
     	System.out.println(Arrays.toString(source_dest) +" : "+ Arrays.toString(setToExclude));
-		int[] dest_source = new int[source_dest.length];
-		dest_source[0] = source_dest[1];
-		dest_source[1] = source_dest[0];
-    	System.out.println(Arrays.toString(dest_source) +" : "+ Arrays.toString(setToExclude));
+		//int[] dest_source = new int[source_dest.length];
+		//dest_source[0] = source_dest[1];
+		//dest_source[1] = source_dest[0];
+    	//System.out.println(Arrays.toString(dest_source) +" : "+ Arrays.toString(setToExclude));
     }
 
 }
